07420a6aed6556148e5123bb248aa77bcdd4fc1b
Anton Antonov <antonov.anton@ukr.net>
2023-05-23 11:59:53 +0300

    Добавьте файлы проекта.

---
+++ URL_Shortener_Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace URL_Shortener_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
